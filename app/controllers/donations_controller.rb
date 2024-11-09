a863ef0ebaf6d452b880737057a3d72a43075fd8
ymstazaz <r37a45d@gmail.com>
2024-11-09 23:00:08 +0900

    formオブジェクトの実装練習

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,30 @@
+class DonationsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  def index
+  end
+
+  def new
+    @donation_address = DonationAddress.new
+  end
+
+  def create
+    @donation_address = DonationAddress.new(donation_params)
+    if @donation_address.valid?
+      @donation_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :price).merge(user_id: current_user.id)
+  end
+
+  # def address_params
+  #   params.permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(donation_id: @donation.id)
+  # end
+
+end
